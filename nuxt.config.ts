7ae85b66486b2fb1d1eac8aeea957cab140b213d
Grover Plaza Quiroga <grover.plaza@gmail.com>
2025-06-05 21:03:14 -0400

    pinia talwind ready

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['@pinia/nuxt'],
 })
